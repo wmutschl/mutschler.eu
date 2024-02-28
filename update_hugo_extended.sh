0fb884ce7f8822eab99b3232bbdbfc866c5fcebe
Willi Mutschler <willi@mutschler.eu>
2024-02-28 11:24:31 +0100

    Update hugo extended script

--- update_hugo_extended.sh
+++ update_hugo_extended.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #export HUGOVER=`curl --silent "https://api.github.com/repos/gohugoio/hugo/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 export HUGOVER=0.101.0
-export ARCH=macos-ARM64
+export HUGOVER=0.120.3
+export ARCH=darwin-universal
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGOVER}/hugo_extended_${HUGOVER}_${ARCH}.tar.gz
 tar -xvf hugo_extended_${HUGOVER}_${ARCH}.tar.gz hugo
 rm hugo_extended_${HUGOVER}_${ARCH}.tar.gz
